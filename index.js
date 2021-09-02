a7ff8e7af4945c96bb43b5e08d54bf827883ad9c
tatsuji_ho <longer271271@gmail.com>
2021-09-02 17:13:31 +0900

    popup内でindexへ遷移できるように変更

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 chrome.tabs.query({}, function (tabs) {
     $('#tabnum').text('現在開いているタブの数は[' + tabs.length + ']個です');
-    $("#extention-id").text(chrome.runtime.id);
 
     for (var i in tabs) {
         $('tbody').after('<tr><td>' + tabs[i].title + '</td><td>' + tabs[i].url + '</td></tr>');
     }
-    $('#search-link').attr("href", "chrome-extension://"+chrome.runtime.id+"/index.html");
 });
