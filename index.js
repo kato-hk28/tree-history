1084f66ee4566c19d2401099ba3833a3f6a20ad0
tatsuji_ho <longer271271@gmail.com>
2021-09-02 17:44:59 +0900

    indexで過去10件の履歴を取得して表示

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ chrome.tabs.query({}, function (tabs) {
     $('#tabnum').text('現在開いているタブの数は[' + tabs.length + ']個です');
 
     for (var i in tabs) {
-        $('tbody').after('<tr><td>' + tabs[i].title + '</td><td>' + tabs[i].url + '</td></tr>');
+        $('#tabs').after('<tr><td>' + tabs[i].title + '</td><td>' + tabs[i].url + '</td></tr>');
+    }
+});
+
+var dateCurrent= new Date(), 
+correction = dateCurrent.getTimezoneOffset()*60*1000;
+var now = Date.now();
+var corrected_now = correction + now;
+var from = corrected_now - 3600;
+
+chrome.history.search({text: '', maxResults: 10}, function(data) {
+    for (var i in data) {
+        $('#histories').after('<tr><td>' + data[i].title + '</td><td>' + data[i].url + '</td></tr>');
     }
 });
