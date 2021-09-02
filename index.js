9b1043afa325500193c803c36750544e769de3ec
tatsuji_ho <longer271271@gmail.com>
2021-09-02 18:36:17 +0900

    popupを最後に開いた時以降の履歴が取得されるようになり、履歴以外の要素がコメントアウトされた

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-chrome.tabs.query({}, function (tabs) {
-    $('#tabnum').text('現在開いているタブの数は[' + tabs.length + ']個です');
+// chrome.tabs.query({}, function (tabs) {
+//     $('#tabnum').text('現在開いているタブの数は[' + tabs.length + ']個です');
 
-    for (var i in tabs) {
-        $('#tabs').after('<tr><td>' + tabs[i].title + '</td><td>' + tabs[i].url + '</td></tr>');
-    }
-});
+//     for (var i in tabs) {
+//         $('#tabs').after('<tr><td>' + tabs[i].title + '</td><td>' + tabs[i].url + '</td></tr>');
+//     }
+// });
 
-var dateCurrent= new Date(), 
-correction = dateCurrent.getTimezoneOffset()*60*1000;
-var now = Date.now();
-var corrected_now = correction + now;
-var from = corrected_now - 3600;
+var dateCurrent= new Date();
+var correction = dateCurrent.getTimezoneOffset()*60*1000;
+// var now = Date.now();
+// var corrected_now = correction + now;
+// var from = corrected_now - 3600;
 
-chrome.history.search({text: '', maxResults: 10}, function(data) {
+var from = Number(localStorage.getItem('time'));
+
+chrome.history.search({text: '', startTime: from, maxResults: 1000}, function(data) {
     for (var i in data) {
         $('#histories').after('<tr><td>' + data[i].title + '</td><td>' + data[i].url + '</td></tr>');
     }
+    console.log(from);
 });
