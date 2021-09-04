2d69d332746e0c9a64233d431b653f94a4dc4372
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2021-09-05 02:18:23 +0900

    nodeの追加の処理

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var dateCurrent= new Date();
-var correction = dateCurrent.getTimezoneOffset()*60*1000;
+var dateCurrent = new Date();
+var correction = dateCurrent.getTimezoneOffset() * 60 * 1000;
 // var now = Date.now();
 // var corrected_now = correction + now;
 // var from = corrected_now - 3600;
 
 var from = Number(localStorage.getItem('time'));
-
+console.log(localStorage.getItem("tree_dict_arr"));
+var edge = JSON.parse(localStorage.getItem("tree_dict_arr"));
+console.log(edge);
 var nodes = new vis.DataSet();
-chrome.history.search({text: '', startTime: from, maxResults: 1000}, function(data) {
-    for (var i in data) {
-        var node = { id: data[i].url, label: data[i].title, url: data[i].url };
-        console.log(node);
-        nodes.add(node);
-    }
-    console.log(from);
-    
-});
+for (var i of edge) {
+    console.log(i);
+    var node = { id: i["to"], label: i["title"], url: i["to"] };
+    console.log(node);
+    nodes.add(node);
+}
+
 
 //var nodes = new vis.DataSet([
 //
@@ -28,19 +28,21 @@ chrome.history.search({text: '', startTime: from, maxResults: 1000}, function(da
 //    {id: 7, label: 'G' },
 //    {id: 8, label: 'H' },
 //]);
-var edges = new vis.DataSet([
+
+// var edges = new vis.DataSet([
 //    {from: 1, to: 2, arrows: 'to' },
 //    {from: 1, to: 3, arrows: 'to' },
 //    {from: 3, to: 4, arrows: 'to' },
 //    {from: 6, to: 1, arrows: 'to' },
 //    {from: 7, to: 8, arrows: 'to' },
 //    {from: 8, to: 7, arrows: 'to' },
-]);
+// ]);
+var edges = new vis.DataSet(edge);
 var container = $('#network')[0];
 var data = {
     nodes: nodes,
     edges: edges
-    };
+};
 var options = {
     //physics: false,
     size: 20
@@ -48,8 +50,8 @@ var options = {
 };
 var network = new vis.Network(container, data, options);
 
-network.on("click",function(params){
-    if(params.nodes.length == 1){
+network.on("click", function (params) {
+    if (params.nodes.length == 1) {
         var nodeId = params.nodes[0];
         var node = nodes.get(nodeId);
         window.open(node.url, '_blank');
